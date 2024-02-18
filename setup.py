4060601d755d4272168a964301918524e2ad1280
alber <alberto.biscalchin@gmail.com>
2024-02-18 16:48:15 +0100

    Implementation of GTM

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Generative Topographic Mapping and Analysis Toolkit'
 
 # Read the contents of your README file
